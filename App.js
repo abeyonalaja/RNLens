935844e6b5e226ceac50c78efbe232c9d0d6339a
abey onalaja <abey.onalaja@gmail.com>
2022-12-22 19:05:51 +0000

    fix: package versions

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Profiles } from '@lens-protocol/react-native-lens-ui-kit'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Profiles } from "@lens-protocol/react-native-lens-ui-kit";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-   <Profiles />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Profiles" component={ProfileComponent} />
+        {/*<Stack.Screen name="Details" component={Details} />*/}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const ProfileComponent = ({ navigation }) => {
+  return <Profiles />;
+};
+
+export default App;
