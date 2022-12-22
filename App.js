ee31dba27c501e8afad579981908e302756fbd21
abey onalaja <abey.onalaja@gmail.com>
2022-12-22 20:35:44 +0000

    add click through from profiles page rto view a single profile

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Profiles } from "@lens-protocol/react-native-lens-ui-kit";
+import ProfileView from "./ProfileView";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,14 +10,19 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Profiles" component={ProfileComponent} />
-        {/*<Stack.Screen name="Details" component={Details} />*/}
+
+        <Stack.Screen name="Profile" component={ProfileView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 const ProfileComponent = ({ navigation }) => {
-  return <Profiles />;
+  return (
+    <Profiles
+      onProfilePress={(profile) => navigation.navigate("Profile", { profile })}
+    />
+  );
 };
 
 export default App;
--- ProfileView.js
+++ ProfileView.js
@@ -4,21 +4,23 @@ const ProfileView = ({ route, navigation }) => {
   return (
     <Profile
       profile={route.params.profile}
-      onFollowingPress={(profile) => {
-        console.log("PROFILE:::: ", profile);
-        navigation.push("ViewFollowing", { ethereumAddress: profile.ownedBy });
-      }}
-      onProfileImagePress={(publication) => {
-        navigation.push("ViewProfile", {
-          profile: publication.profile,
-        });
-      }}
-      onCommentPress={(publication) => {
-        const publicationId = publication.mirrorOf
-          ? publication.mirrorOf.id
-          : publication.id;
-        navigation.push("ViewComments", { publicationId });
-      }}
+      // onFollowingPress={(profile) => {
+      //   console.log("PROFILE:::: ", profile);
+      //   navigation.push("ViewFollowing", { ethereumAddress: profile.ownedBy });
+      // }}
+      // onProfileImagePress={(publication) => {
+      //   navigation.push("ViewProfile", {
+      //     profile: publication.profile,
+      //   });
+      // }}
+      // onCommentPress={(publication) => {
+      //   const publicationId = publication.mirrorOf
+      //     ? publication.mirrorOf.id
+      //     : publication.id;
+      //   navigation.push("ViewComments", { publicationId });
+      // }}
     />
   );
 };
+
+export default ProfileView;
