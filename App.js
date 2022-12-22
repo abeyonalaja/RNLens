4f27a81dd23059b9fb926374db51c87741cd4d35
abey onalaja <abey.onalaja@gmail.com>
2022-12-22 18:35:30 +0000

    Add profile component

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Profiles } from '@lens-protocol/react-native-lens-ui-kit'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Profiles />
   );
 }
 
